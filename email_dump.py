333c9f0a60e1e59abf33044ede56dbda09a4bfaa
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-31 19:23:02 +0530

    download all the attachment from the mail

--- email_dump.py
+++ email_dump.py
@@ -24,8 +24,10 @@ SOFTWARE.
 from __future__ import annotations
 
 import argparse
+import email
 import getpass
 import imaplib
+import os
 from pathlib import Path
 from types import TracebackType
 from typing import Sequence
@@ -45,15 +47,20 @@ def _check_dir(dirpath: str) -> str:
 
 
 def _prompt_email_password() -> tuple[str, str]:
-    email = input('Email: ')
+    email_add = input('Email: ')
     password = getpass.getpass()
-    return email, password
+    return email_add, password
+
+
+def _save_file(filepath: str, content: bytes) -> None:
+    with open(filepath, 'wb') as f:
+        f.write(content)
 
 
 class Email:
-    def __init__(self, email: str, password: str) -> None:
+    def __init__(self, email_add: str, password: str) -> None:
         self.conn = imaplib.IMAP4_SSL('imap.gmail.com')
-        self.email = email
+        self.email_add = email_add
         self.password = password
 
     def __enter__(self) -> Email:
@@ -65,7 +72,7 @@ class Email:
         pass
 
     def login(self) -> None:
-        self.conn.login(self.email, self.password)
+        self.conn.login(self.email_add, self.password)
 
     def get_emails_from_user(self, from_email: str) -> list[imaplib._AnyResponseData]:
         self.conn.select('Inbox')
@@ -79,8 +86,29 @@ class Email:
 
         return emails
 
-    def save_email_from_user(self, from_user: str, dir: str) -> None:
-        pass
+    def save_email_from_user(self, from_email: str, dir: str) -> None:
+        emails = self.get_emails_from_user(from_email=from_email)
+
+        for idx, e in enumerate(emails):
+            Path(os.path.join(dir, str(idx))).mkdir(exist_ok=True)
+
+            if not isinstance(e, tuple):
+                return
+
+            raw_email = e[0][1]
+            raw_email_string = raw_email.decode('utf-8')
+            email_message = email.message_from_string(raw_email_string)
+
+            for part in email_message.walk():
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+                filename = part.get_filename()
+                if bool(filename):
+                    file_path = os.path.join(dir, str(idx), filename)
+                    _save_file(filepath=file_path,
+                               content=part.get_payload(decode=True))
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -103,12 +131,11 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
 
     download_dir = args.dir if args.dir else '.'
-    print(download_dir)
 
     if args.email is None or args.password is None:
         args.email, args.password = _prompt_email_password()
 
-    with Email(args.email, args.password) as email:
-        print(email.get_emails_from_user(vars(args)['from']))
+    with Email(args.email, args.password) as mail:
+        mail.save_email_from_user(vars(args)['from'], download_dir)
 
     return 0
