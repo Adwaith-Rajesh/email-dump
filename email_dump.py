9f115ab7f00139ed9dcbcb1ebfdac5b992a8828a
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-31 17:25:12 +0530

    Added the arguments to the argument parser

--- email_dump.py
+++ email_dump.py
@@ -1,3 +1,55 @@
-def main() -> int:
-    print('Hello World')
+"""
+MIT License
+
+Copyright (c) 2023 Adwaith Rajesh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import argparse
+from pathlib import Path
+from typing import Sequence
+
+EXAMPLE_USE = '''
+Example Use:
+    email-dump --from example-email@email.com --dir dir/to/dump
+    email-dump --from example-email@email.com
+'''
+
+
+def _check_dir(dirpath: str) -> str:
+    if Path(dirpath).is_dir():
+        return dirpath
+    raise NotADirectoryError(dirpath)
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser(
+        description='dump all the email sent by a specific user',
+        epilog=EXAMPLE_USE,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('--from', '-f', type=str,
+                        help='email of the user to dump from.', required=True)
+    parser.add_argument('--dir', '-d', type=_check_dir,
+                        help='The directory to dump all the emails to.')
+    args = parser.parse_args()
+    print(args)
+
     return 0
